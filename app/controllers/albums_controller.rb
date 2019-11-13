78e1b7c98709fee4abf032b9d3f9c60a7fb26aa7
Colin Jones <colintjones@gmail.com>
2019-11-13 16:10:34 -0500

    Added create, show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,8 +1,22 @@
 class AlbumsController < ApplicationController
+
+
+    def show
+        @album = Album.find(params[:id])
+    end
+
     def new
     end
 
     def create
-        render plain: params[:album].inspect
+        @album = Album.new(album_params)
+
+        @album.save
+        redirect_to @album
     end
+
+    private
+        def album_params
+            params.require(:album).permit(:album_name, :artist, :year)
+        end
 end
